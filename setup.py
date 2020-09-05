884ff9e83177ac349a165ec95ccf6e9f63e8487f
theo2021 <theo2021@gmail.com>
2020-09-05 20:42:21 +0200

    recommit

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='python_nn',
@@ -9,5 +9,6 @@ setup(
     author_email='theo2021@gmail.com',
     license='unlicense',
     packages=['python_nn'],
-    zip_safe=False
+    zip_safe=False,
+    packages=find_packages()
 )
