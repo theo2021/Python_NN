33ace95091d9055cbc0d2ec6eff5557fd4047f82
theo2021 <theo2021@gmail.com>
2020-09-05 19:48:44 +0200

    recommit

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='python_nn',
     version='0.1.1',
     description='A simple python neural network implementation',
-    url='git@github.com:theo2021/Python_NN.git',
+    url='https://github.com/theo2021/Python_NN',
     author='theodoros Panagiotakopoulos',
     author_email='theo2021@gmail.com',
     license='unlicense',
