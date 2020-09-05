bf1a4930a4a9740cb5bad5227a85321d656beb21
theo2021 <theo2021@gmail.com>
2020-09-05 20:04:40 +0200

    fixing init

--- python_nn/__init__.py
+++ python_nn/__init__.py
@@ -5,11 +5,3 @@ Created on Wed May 27 19:34:30 2020
 @author: Theodor Panagiotakopoulos
 """
 
-
-from . import layers
-from . import functions
-from . import models
-from . import optimizers
-from . import initializers
-from . import callbacks
-
